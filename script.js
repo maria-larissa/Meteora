ca385203877569987331bdd0b5044894a13ce066
larissa <larissa@debian-BULLSEYE-live-builder-AMD64>
2023-06-21 12:32:34 -0300

    Header, search bar, news banner ready!

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ btnFecharMenu.onclick = function(){
     navMenuMobile.style.display = "none";
     btnFecharMenu.style.display = "none";
 }
+
+
+
+// Banner
+const fotoBannerNovidades = document.getElementById("foto-banner");
+let i = 0;
+let caminhoImagens = ["assets/images/"]
+function atualizaFotoBannerNovidades(){
+    switch(i){
+        case 0:
+            fotoBannerNovidades.src = "assests/"
+    }
+}
